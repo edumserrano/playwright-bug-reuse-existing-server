8236c3e5b7f79a520ef119bd02fe39a343724bba
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2023-07-07 18:55:24 +0100

    Allow webserver connectivity between docker and host

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig, devices } from '@playwright/test';
 
+
+const webServerPort = 4200;
+const host = process.env.USE_DOCKER_HOST_WEBSERVER ? `host.docker.internal` : `127.0.0.1`;
+const webServerUrl = `http://${host}:${webServerPort}`;
+
+console.log(`WebServer URL set to ${webServerUrl}`);
+
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
@@ -13,8 +20,8 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: `npx ng serve --port 4200`,
-    url: 'http://127.0.0.1:4200',
+    command: `npx ng serve --port ${webServerPort}`,
+    url: webServerUrl,
     reuseExistingServer: true,
     stdout: "pipe",
     timeout: 30 * 1000, // 30 secs
